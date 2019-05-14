a11fc3817a790b95f1f9d75bf99afd95d75c1ae4
Pilar Argudo <pargudo@disid.com>
2019-05-14 11:56:31 +0200

    Add di container and up

--- public/index.php
+++ public/index.php
@@ -1,17 +1,13 @@
 <?php
 
 ini_set('max_execution_time', 0);
-
 // cogemos directorio base raíz dónde arranca el proyecto
 require_once dirname( __DIR__ ).'/vendor/autoload.php';
 
 use App\kernel;
-
 use Kint;
 
 $kernel = new kernel();
-
 Kint::dump($GLOBALS);
-
 $kernel->init();
 
--- src/kernel.php
+++ src/kernel.php
@@ -4,6 +4,10 @@ namespace App;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
+use DI\ContainerBuilder;
+
+use Kint;
+
   class kernel
   {
 
@@ -14,6 +18,9 @@ use Monolog\Logger;
       $this->logger = new Logger('Kernel');
       $this->logger->pushHandler(new StreamHandler(dirname(__DIR__).'/var/log/prod.log'), Logger:: DEBUG);
 
+      // create container builder with php-di
+      $this->container = $this->createContainer();
+      Kint::dump($this->container);
     }
 
     public function init()
@@ -22,5 +29,19 @@ use Monolog\Logger;
       // monolog
       $this->logger->info('Kernel up');      
       echo "<h1>Arrancado kernel</h1>";
+    }
+
+    // librería PHP-DI
+    // create container builder
+    public function createContainer()
+    {
+      // creamos la instancia
+      $containerBuilder = new ContainerBuilder();
+      // definimos la instancia, 
+      // por el nombre de la variable va a ser capaz de inyectarme el objeto de esa variable automáticamente
+      // autowiring true
+      $containerBuilder->useAutowiring(true);
+      // Create container
+      return $containerBuilder->build();
     }
   }
