8896452b6f7aebe784fe60201ee80e3e6728154c
Pilar Argudo <pargudo@disid.com>
2019-05-16 10:02:29 +0200

    Improved method not allowed

--- src/RoutingManager.php
+++ src/RoutingManager.php
@@ -22,13 +22,17 @@ class RoutingManager
     switch($route[0]) {
       case FastRoute\Dispatcher::NOT_FOUND:
           header("HTTP/1.0 404 Not Found");
-          echo "<h1> NOT FOUND </h1>";
+          echo '<h1> NOT FOUND </h1>';
+          break;
+      case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:  //constante estática
+          header("HTTP/1.0 405 Method not allowed");
+          echo '<h1> PETICIÓN NO PERMITIDA </h1>'; // todo lo que no esté definido en el enrutado Web.php saldrá este mensaje
           break;
       case FastRoute\Dispatcher::FOUND:  
           $controller = $route[1];
           $params =  $route[2];  
           $this->container->call($controller, $params);
-          break;
+          break;          
 
     }
 
